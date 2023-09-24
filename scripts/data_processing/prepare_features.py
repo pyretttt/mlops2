27a6139f1ac706a78010e3a58cb0cdd0225456c2
jack sgory <pyretttt>
2023-09-24 17:03:35 +0400

    “Baseline”

--- scripts/data_processing/prepare_features.py
+++ scripts/data_processing/prepare_features.py
@@ -8,6 +8,8 @@ dataset['sepal.length'] = dataset['sepal.length'].astype(float)
 dataset['sepal.width'] = dataset['sepal.width'].astype(float)
 dataset['petal.length'] = dataset['petal.length'].astype(float)
 dataset['petal.width'] = dataset['petal.width'].astype(float)
+mapper = {'Virginica':0, 'Versicolor':2, 'Setosa':3}
+dataset['variety'] = dataset['variety'].map(lambda x: mapper[x])
 
 os.makedirs(os.path.join("data", "prepared"), exist_ok=True)
 dataset.to_csv('data/prepared/prepared.csv')
